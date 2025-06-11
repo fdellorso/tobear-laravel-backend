faf37712011498c2e6a4f4ae6614d9fa0418c7cd
Francesco Dell'Orso <francesco.dellorso@gmail.com>
2025-06-11 21:12:14 +0200

    new test-mail route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExecuteArtisanCommandController;
 // use App\Http\Controllers\FileController;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/laravelversion', function () {
     return ['Laravel' => app()->version()];
@@ -15,6 +16,17 @@ Route::get('/serverphpinfo', function () {
 Route::get('/artisan/{name_of_command}', ExecuteArtisanCommandController::class);
 
 // Route::get('/assets/{filename}', [FileController::class, 'show']);
+
+Route::get('test-email', function () {
+    try {
+        Mail::raw('Test SMTP x10', function ($msg) {
+            $msg->to('fdellorso@ymail.com')->subject('Prova SMTP');
+        });
+        return 'Email inviata con successo!';
+    } catch (\Exception $e) {
+        return 'Errore nell\'invio dell\'email: '.$e->getMessage();
+    }
+});
 
 Route::get('/{any}', function () {
     return file_get_contents(public_path('app/index.html'));
